69a5702435381e4636117fadffcd69ea27b9b6ac
KimGranaderos <kchangranaderos@gmail.com>
2020-11-20 23:02:22 +0800

    Modified Deleted idk

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome!" />
-    </div>
+  <v-app id="inspire">
+    <v-app-bar flat app class="grey lighten-5">
+      <v-toolbar flat class="grey lighten-5">
+        <v-toolbar-title class="px-16">
+          <v-img
+              :src="require('@/assets/fftpicblack.png')"
+              contain
+              max-height="55"
+              max-width="125"
+          ></v-img>
+        </v-toolbar-title>
+      </v-toolbar>
+    </v-app-bar>
+
+    <v-main class="grey lighten-5">
+      <Dashboard />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Dashboard from './components/Dashboard'
 
 export default {
-    name: "App",
-    components: {
-        HelloWorld,
-    },
+  components: {
+    Dashboard
+  }
 };
 </script>
-
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
