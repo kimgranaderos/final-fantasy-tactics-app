27db2ab63e79384e092076e630e2ba18b2db0d8b
KimGranaderos <kchangranaderos@gmail.com>
2020-11-20 22:55:53 +0800

    Added Plugins

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,24 @@
+import '@mdi/font/css/materialdesignicons.css'
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+      iconfont: "mdi",
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: '#212121',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+      },
+    },
+  },
+});
